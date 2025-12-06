00f2dd9b441d2d46ff3d53852bcfacf78f8376ef
Manuel Riedel <manuelriedel002@gmail.com>
2025-12-06 20:54:40 -0300

    Pedido DTO se movió a la carpeta correspondiente

--- src/application/dtos/PedidoDTO.js
+++
@@ -1,9 +0,0 @@
-class PedidoDTO {
-  constructor({ id, fechaEmision, fechaEntrega, repartoId }) {
-    this.id = id;
-    this.fechaEmision = fechaEmision;
-    this.fechaEntrega = fechaEntrega;
-    this.repartoId = repartoId;
-  }
-}
-module.exports = PedidoDTO;
